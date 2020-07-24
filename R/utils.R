5c6112a325648fc156f08318199752c0612320a0
Pascal Ledru <pascal.ledru@oracle.com>
2020-07-23 21:24:31 -0600

    initial commit

---
+++ R/utils.R
@@ -0,0 +1,9 @@
+#' Make ATAC CDS object
+#'
+#' This function takes as input a data frame or a path to a file in a sparse
+#' matrix format and returns a properly formatted \code{CellDataSet} (CDS)
+#' object.
+#'
+test_it <- function() {
+    print("test it...")
+}
