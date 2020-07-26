594dbe476e485a522847d4c18603890fae0a511f
Pascal Ledru <pascal.ledru@oracle.com>
2020-07-26 16:20:36 -0600

    move documentation for x to data.R

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,3 @@
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
-#'
-#' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-#' @source \url{http://www.diamondse.info/}
-"x"
-
 #' Make ATAC CDS object
 #'
 #' This function takes as input a data frame or a path to a file in a sparse
