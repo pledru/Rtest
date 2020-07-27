981347f04db1fd89f1616654f9214af606972a87
Pascal Ledru <pascal.ledru@oracle.com>
2020-07-27 11:22:35 -0600

    example of a hashmap

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,16 @@
+#' a track object.
+track <- setClass("track", slots = c(x="numeric", y="numeric"))
+## an object from the class
+t1 <- track(x = 1:10, y = 1:10 + rnorm(10))
+
+## A class extending the previous, adding one more slot
+trackCurve <- setClass("trackCurve",
+    slots = c(smooth = "numeric"),
+    contains = "track")
+
+## an object containing a superclass object
+t1s <- trackCurve(t1, smooth = 1:10)
+
 #' Make ATAC CDS object
 #'
 #' This function takes as input a data frame or a path to a file in a sparse
@@ -7,4 +20,19 @@
 #' @export
 test_it <- function() {
     print("test it...")
+}
+
+#' Test a hashmap
+#'
+#' This function tests a hashmap.
+#'
+#' @export
+test_hashmap <- function() {
+  x <- c(1,2,3,4)
+  y <- c("1", "2", "3", "4")
+
+  h <- hashmap(x, y)
+  class(h)
+
+  h$insert(8, "8")
 }
