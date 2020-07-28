18f353994a91ce76ff50f308ebd38e92a17bae34
Pascal Ledru <pascal.ledru@oracle.com>
2020-07-27 18:54:58 -0600

    lint error

--- R/utils.R
+++ R/utils.R
@@ -2,14 +2,15 @@
 #' @importFrom methods setClass
 #' @importFrom stats rnorm
 test_zero <- function() {
-  track <- setClass("track", slots = c(x="numeric", y="numeric"))
+  track <- setClass("track", slots = c(x = "numeric", y = "numeric"))
   t1 <- track(x = 1:10, y = 1:10 + rnorm(10))
 
-  trackCurve <- setClass("trackCurve",
+  track_curve <- setClass("track_curve",
       slots = c(smooth = "numeric"),
       contains = "track")
 
-  t1s <- trackCurve(t1, smooth = 1:10)
+  t1s <- track_curve(t1, smooth = 1:10)
+  t1s
 }
 
 #' Make ATAC CDS object
@@ -30,7 +31,7 @@ test_it <- function() {
 #' @importFrom hashmap hashmap
 #' @export
 test_hashmap <- function() {
-  x <- c(1,2,3,4)
+  x <- c(1, 2, 3, 4)
   y <- c("1", "2", "3", "4")
 
   h <- hashmap(x, y)
@@ -46,11 +47,11 @@ test_hashmap <- function() {
 #' @importFrom ggplot2 aes ggplot geom_point
 #' @export
 test_graph <- function() {
-  x <- sample(1:10, 20, replace=TRUE)
-  y <- sample(1:10, 20, replace=TRUE)
+  x <- sample(1:10, 20, replace = TRUE)
+  y <- sample(1:10, 20, replace = TRUE)
 
   states <- c("AL", "AK", "AR", "CA", "CO", "MA", "MO", "NC", "NY", "RI")
-  s <- sample(states, 20, replace=TRUE)
+  s <- sample(states, 20, replace = TRUE)
 
   data <- data.frame(
     name = s,
